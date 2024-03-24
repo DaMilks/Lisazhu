11c3598ebe88c0f946429a511d82d5c88280b465
DaMilks <danya.boldyrev.89@mail.ru>
2024-03-24 10:58:21 +0300

    Добавьте файлы проекта.

---
+++ Lisazhu/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lisazhu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int NumPoints = 200;
+            double delta = 2 * Math.PI / NumPoints;
+            double phaseX = 0.0, phaseY = 0.0;
+            double freqX = Convert.ToDouble(textBox1.Text), freqY = Convert.ToDouble(textBox2.Text);
+            chart1.Series[0].Points.Clear();
+            for (int i = 0; i < NumPoints; i++)
+            {
+                double x = Math.Sin(freqX * phaseX);
+                double y =Math.Sin(freqY * phaseY);
+                phaseX += delta;
+                phaseY += delta;
+
+                chart1.Series[0].Points.AddXY(x, y);
+            }
+        }
+    }
+}
